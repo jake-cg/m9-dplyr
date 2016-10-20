aa909c14114f0f178ef7a9511bc97ea0b6113594
Jake George <Jake George>
2016-10-20 14:38:57 -0700

    Completed Exercise 4

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -7,27 +7,29 @@ library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights <- mutate(flights, time_gained = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, -time_gained)
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- mutate (flights, time_gained = arr_delay - dep_delay) %>%
+  arrange(-time_gained)
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$time_gained)
 
 # On average, did flights gain or lose time?
-
+mean <= mean(flights$time_gained, na.rm = TRUE)
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
-
+sea.tac <- filter(flights, dest == "SEA")
 
 # On average, did flights to seatac gain or loose time?
+mean.sea.tac <- mean(sea.tac$time_gained, na.rm = TRUE)
 
 ### Bonus ###
 # Write a function that allows you to specify an origin, a destination, and a column of interest
